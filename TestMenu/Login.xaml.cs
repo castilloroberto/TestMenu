b5cf879bf865a532b1e65e6135aa41d2be76d2e8
robertocastillo <robertocastillo945@gmail.com>
2021-06-02 13:07:00 -0600

    Agregar archivos de proyecto.

---
+++ TestMenu/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestMenu
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_logear_Click(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ TestMenu/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void txt_search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_search.Foreground.ToString() == "#FF808080")
+            {
+                txt_search.Text = "";
+                txt_search.Foreground = new SolidColorBrush(Colors.Black);
+
+            }
+           
+        }
+
+        private void txt_search_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+            if (txt_search.Text.Length == 0)
+            {
+                txt_search.Foreground = new SolidColorBrush(Colors.Gray);
+                txt_search.Text = "Nombre o Codigo de Producto";
+
+            }
+        }
+    }
+}
